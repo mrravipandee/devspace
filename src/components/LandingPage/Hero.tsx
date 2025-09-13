83ed4de74dc68cbe272c7d02ad2628245e34328e
Ravi Pandey <imravipanday@gmail.com>
2025-09-14 00:38:26 +0530

    Home page

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -97,45 +97,7 @@ export default function Hero() {
                     </motion.div>
                 </div>
 
-                {/* Animated Images - Left and Right Content */}
-                <div className="hidden md:block absolute inset-0 overflow-hidden pointer-events-none">
-                    {/* Left Content - Will flip and move left */}
-                    <motion.div
-                        style={{
-                            rotateY: leftFlip,
-                            x: leftX,
-                            opacity: leftOpacity,
-                            perspective: '1000px',
-                            transformStyle: 'preserve-3d'
-                        }}
-                        className="absolute top-[15%] left-[-80px] w-[400px]"
-                    >
-                        <Image
-                            src="/devspace_adarsh.png"
-                            height={500}
-                            width={500}
-                            className="-rotate-12"
-                            alt="Adarsh dubey Review"
-                        />
-                    </motion.div>
-
-                    {/* Right Content - Will zoom in */}
-                    <motion.div
-                        style={{
-                            scale: rightScale,
-                            opacity: rightOpacity,
-                        }}
-                        className="absolute top-[15%] right-[5%] w-[220px] origin-center"
-                    >
-                        <Image
-                            src="/user_in_frame.png"
-                            height={400}
-                            width={200}
-                            className="rounded-[30px] object-cover shadow-xl h-[30rem]"
-                            alt="Phone user mode right"
-                        />
-                    </motion.div>
-                </div>
+                
             </section>
 
             {/* Modal */}
